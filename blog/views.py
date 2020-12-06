df2b92e7614a88a4c9e8dad7d27762921bd6acc6
Hardik Kalaria <hardikkalaria@gmail.com>
2020-12-06 22:46:57 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_blogs_list'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
